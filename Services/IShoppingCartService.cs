bc94d6abf9f129f0212641f4b1f8d0d231520bfa
geethasamynathan <geethasamynathan2011@gmail.com>
2023-09-15 09:18:35 +0530

    web api with Xunit

---
+++ Services/IShoppingCartService.cs
@@ -0,0 +1,12 @@
+﻿using ShoppingCartAPI.Models;
+
+namespace ShoppingCartAPI.Services
+{
+    public interface IShoppingCartService
+    {
+        IEnumerable<ShoppingItem> GetAllItems();
+        ShoppingItem Add(ShoppingItem newItem);
+        ShoppingItem GetById(Guid id);
+        void Remove(Guid id);
+    }
+}
---
+++ Services/ShoppingCartService.cs
@@ -0,0 +1,12 @@
+﻿using ShoppingCartAPI.Models;
+
+namespace ShoppingCartAPI.Services
+{
+    public class ShoppingCartService//:IShoppingCartService
+    {
+        //public ShoppingItem Add(ShoppingItem newItem) => throw new NotImplementedException();
+        //public IEnumerable<ShoppingItem> GetAllItems() => throw new NotImplementedException();
+        //public ShoppingItem GetById(Guid id) => throw new NotImplementedException();
+        //public void Remove(Guid id) => throw new NotImplementedException();
+    }
+}
